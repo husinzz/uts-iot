ba48b0c89f512b4ef371d2a8aaaeb23c38f09efa
husinzz <its.husin@gmail.com>
2021-04-13 23:35:40 +0700

    final udah selesai

--- uts-final.py
+++ uts-final.py
@@ -1,7 +1,9 @@
 import pandas as pd;
 import matplotlib.pyplot as plt;
 import math;
+
 from scipy.integrate import quad;
+from scipy.signal import butter, lfilter, filtfilt
 
 dataGyro = pd.DataFrame(pd.read_csv('gyro.csv').rolling(window=1).mean())
 dataAcel = pd.DataFrame(pd.read_csv('acel.csv').rolling(window=1).mean())
@@ -16,9 +18,20 @@ def plotRawdata(): # Soal 3
         plt.subplot(3,2,2*i) 
         plt.plot(dataGyro.iloc[:,i], label=l[i-1])
         plt.legend(loc='upper left');
-    plt.show();
 
 # Bagian soal 4
+
+fs = 100; 
+nyq = 0.5 * fs 
+wn = 2/nyq; 
+
+def lowpass(var): # PERLU JANGAN HAPUS LAGI SIN
+    b, a = butter(2, wn, btype='low');
+    return filtfilt(b, a, var);
+
+def highpass(var): # PERLU JANGAN HAPUS LAGI SIN
+    b,a = butter(2, wn, btype='high'); # highpass filter untuk data gyroscop
+    return filtfilt(b, a, var); 
 
 def findAtan(): # Soal 4
     atan = []
@@ -29,7 +42,7 @@ def findAtan(): # Soal 4
 def integrals():
     gral = []
     for i in range(len(dataGyro)):
-        ans, err = quad(lambda x : dataAcel.iloc[i]['Linear Acceleration z (m/s^2)'],0,1)
+        ans, err = quad(lambda x : dataGyro.iloc[i]['Gyroscope z (rad/s)'],0,1)
         gral.append(ans)
     return gral
 
